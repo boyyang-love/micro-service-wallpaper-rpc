3a8ef7048f69e8f764bd6d258c32626c2b9020de
boyyang <boyyang-love@outlook.com>
2025-03-30 12:06:47 +0800

    feat: 修改 :sparkles:
    修改

--- upload/helper/minio.go
+++ upload/helper/minio.go
@@ -20,3 +20,15 @@ func Minio(endpoint string, accessKey string, secretAccessKey string, secure boo
 
 	return minioClient, nil
 }
+
+func MinioCore(endpoint string, accessKey string, secretAccessKey string, secure bool) (minioCore *minio.Core, err error) {
+	minioCore, err = minio.NewCore(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKey, secretAccessKey, ""),
+		Secure: secure,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return minioCore, nil
+}
--- upload/internal/svc/servicecontext.go
+++ upload/internal/svc/servicecontext.go
@@ -13,6 +13,7 @@ type ServiceContext struct {
 	Config      config.Config
 	DB          *gorm.DB
 	MinioClient *minio.Client
+	MinioCore   *minio.Core
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -36,9 +37,22 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		fmt.Println("对象存储连接成功")
 	}
 
+	minioCore, err := helper.MinioCore(
+		c.MinioClientConf.Endpoint,
+		c.MinioClientConf.AccessKey,
+		c.MinioClientConf.SecretKey,
+		c.MinioClientConf.Secure,
+	)
+	if err != nil {
+		fmt.Printf("minioCore:对象存储连接失败(%s)\n", err.Error())
+	} else {
+		fmt.Println("minioCore:对象存储连接成功")
+	}
+
 	return &ServiceContext{
 		Config:      c,
 		DB:          db,
 		MinioClient: minioClient,
+		MinioCore:   minioCore,
 	}
 }
