71f88f5df825344778f87cdcaab4a7e4beaafe66
boyyang <62252841+boyyang-love@users.noreply.github.com>
2025-08-29 12:27:01 +0800

    feat: 修改文件上传rpc :sparkles:

--- upload/internal/logic/cosuploadlogic.go
+++ upload/internal/logic/cosuploadlogic.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/boyyang-love/micro-service-wallpaper-rpc/upload/helper"
+	"strings"
 
 	"github.com/boyyang-love/micro-service-wallpaper-rpc/upload/internal/svc"
 	"github.com/boyyang-love/micro-service-wallpaper-rpc/upload/pb/upload"
@@ -33,16 +34,12 @@ func (l *CosUploadLogic) CosUpload(in *upload.ImageUploadReq) (*upload.ImageUplo
 
 	imgOriPath := fmt.Sprintf("%s/%s", in.BucketName, in.OriPath)
 	imgComPath := fmt.Sprintf("%s/%s", in.BucketName, in.Path)
+
 	_, err = l.
 		svcCtx.
 		CosClient.
 		Object.
-		Put(
-			l.ctx,
-			imgComPath,
-			comp.Buf,
-			nil,
-		)
+		Put(l.ctx, imgComPath, comp.Buf, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -51,13 +48,17 @@ func (l *CosUploadLogic) CosUpload(in *upload.ImageUploadReq) (*upload.ImageUplo
 		svcCtx.
 		CosClient.
 		Object.
-		Put(
-			l.ctx,
-			imgOriPath,
-			comp.OriBuf,
-			nil,
-		)
+		Put(l.ctx, imgOriPath, comp.OriBuf, nil)
+	if err != nil {
+		return nil, err
+	}
 
+	oriInfo, err := l.svcCtx.CosClient.Object.Head(l.ctx, imgOriPath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	comInfo, err := l.svcCtx.CosClient.Object.Head(l.ctx, imgComPath, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -70,8 +71,8 @@ func (l *CosUploadLogic) CosUpload(in *upload.ImageUploadReq) (*upload.ImageUplo
 		Data: &upload.ImageUploadResData{
 			Path:    in.Path,
 			OriPath: in.OriPath,
-			ETag:    helper.MakeImageFileHashByBytes(comp.Buf.Bytes()),
-			OriETag: helper.MakeImageFileHashByBytes(comp.OriBuf.Bytes()),
+			ETag:    strings.ReplaceAll(comInfo.Header.Get("ETag"), "\"", ""),
+			OriETag: strings.ReplaceAll(oriInfo.Header.Get("ETag"), "\"", ""),
 			Size:    uint64(comp.Size),
 			OriSize: uint64(comp.OriSize),
 		},
